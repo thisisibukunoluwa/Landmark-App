d954dc15d1b4f0b8a648733ad3d12bc010e5ba74
Ibukunoluwa Akintobi <ibukunoluwaakintobi@gmail.com>
2023-08-18 15:40:32 +0100

    feat:added Landmarks/Landmarks/Views/CircleImage.swift

---
+++ Landmarks/Data.swift
@@ -0,0 +1,8 @@
+//
+//  Data.swift
+//  Landmarks
+//
+//  Created by Ibukunoluwa Akintobi on 18/08/2023.
+//
+
+import Foundation
---
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Ibukunoluwa Akintobi on 18/08/2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
